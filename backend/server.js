80307784e84d90fce8b5dc9b8c358628a223d3d0
unknown <handsomebanana910@gmail.com>
2024-12-15 15:11:35 +0700

    add info for  deploy

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,13 @@ import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import cookieParser from 'cookie-parser';
 import userRoutes from './routes/user.route.js';
+import path from 'path';
 import { app, server } from './socket/socket.js';
 
 dotenv.config();
+
+const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 5000
 
 app.use(express.json()); // to parse the incoming request with JSON payloads 
@@ -18,7 +22,11 @@ app.use("/api/users",userRoutes)
 app.use("/api/message",messageRoutes)
 
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 server.listen( PORT,()=>{
     connectToMongoDB()
     console.log(`run on port ${PORT}`)
